1853791f497dcdd1051e4cc851b5c6ba7ccabbc6
Eli Mallon <eli@livepeer.org>
2019-05-20 13:35:44 -0700

    sudo

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -59,10 +59,10 @@ if [ $(uname) == "Linux" ]; then
   # Check for CUDA and try to install.
   if ! dpkg-query -W cuda-10-0; then
     curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-    dpkg -i ./cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-    apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-    apt-get update
-    apt-get install cuda-10-0 -y
+    sudo dpkg -i ./cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+    sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+    sudo apt-get update
+    sudo apt-get install cuda-10-0 -y
   fi
   EXTRA_FFMPEG_FLAGS="$EXTRA_FFMPEG_FLAGS --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64"
   # Enable persistence mode
