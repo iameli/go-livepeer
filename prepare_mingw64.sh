052d7f6267f8237ec2b245a104aa57f1eab24a43
Livepeer Windows Build <fakeemail@example.com>
2021-03-04 13:11:44 -0800

    ci: adjustments for static windows build

--- prepare_mingw64.sh
+++ prepare_mingw64.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export MINGW_INSTALLS=mingw64
-pacman -S --noconfirm --noprogressbar --ask=20 --needed mingw-w64-x86_64-binutils mingw-w64-x86_64-gcc mingw-w64-x86_64-pkg-config mingw-w64-x86_64-go mingw-w64-x86_64-nasm git make autoconf automake patch libtool texinfo gtk-doc
+pacman -S --noconfirm --noprogressbar --ask=20 --needed mingw-w64-x86_64-binutils mingw-w64-x86_64-gcc mingw-w64-x86_64-pkg-config mingw-w64-x86_64-go mingw-w64-x86_64-nasm mingw-w64-x86_64-clang git make autoconf automake patch libtool texinfo gtk-doc
 gpg --recv-keys F3599FF828C67298 249B39D24F25E3B6 2071B08A33BD3F06 29EE58B996865171 D605848ED7E69871
 
 # winpthreads needs git config for some reason
